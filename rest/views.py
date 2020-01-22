726f72debe3faf38a8bd00abf60d2f893682329c
adorda <angdmz@gmail.com>
2020-01-22 17:52:58 -0300

    view client

--- rest/views.py
+++ rest/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 
 from clients.models import Client
+from clients.serializers import ClientSerializer
 
 
 class ClientViewSet(ModelViewSet):
-    permission_classes = (JWTAuthentication, IsAuthenticated, )
+    serializer_class = ClientSerializer
+    authentication_classes = (JWTTokenUserAuthentication ,)
+    permission_classes = (IsAuthenticated, )
     queryset = Client.objects.all()
